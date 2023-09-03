5104879f8097d6513293832bea58ed98ef73a139
Ilahe Samedova <ilahesamedova@mail.ru>
2023-09-03 16:44:45 +0400

    Initial Commit

---
+++ Millionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Millionaire
+//
+//  Created by Ilahe Samedova on 03.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
